50654eae25b2622239aa1fcb6a026b472b4c823f
DavidRood <shakiel@roadrunner.com>
2017-10-16 19:58:33 -0400

    local host start and client should join

---
+++ Assets/_Scripts/MyNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Starting Host.");
+        StartHost();
+        
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Host Started.");
+    }
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client Start Request.");
+        // base.OnStartClient(myClient);
+        InvokeRepeating("PrintDots", 0f, 1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+        CancelInvoke();
+        // base.OnClientConnect(conn);
+    }
+
+    void PrintDots()
+    {
+        Debug.Log(".");
+    }
+}
